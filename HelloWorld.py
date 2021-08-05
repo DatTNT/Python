89fb22c1cc1c1b042c4b5425c460b2b56db192d8
TienDat <dattnt24@gmail.com>
2021-08-05 22:35:57 +0700

    HelloWorldAndVariablePython

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello World Dat");
+    x = 'How are you';
+    y = x.upper();
+    print(y);
+ 
+if __name__ == "__main__":
+    main()
+ 
---
+++ Variable.py
@@ -0,0 +1,26 @@
+from typing import Counter
+
+
+def main():
+    # dasdasd Không cần kiểu khai báo biến như java hoặc các ngôn ngữ khác
+    Counter = 100;
+    miles = 1000.0;
+    name = "TienDat";
+    print(Counter);
+    print(miles);
+    print(name);
+    # Multiple Assignment Nếu 2 phần tử giống nhau cùng khai bảo thì python sẻ lấy phần tử cuối cùng
+    a = b = c = 1;
+    z,x,c = 1,2,"TienDat";
+    print(a,b,c);
+
+    #Multi-Line Stament ( "/")
+    total = item_one + \
+            item_two + \
+            item_three
+
+    # Quotation in Python
+    word
+
+if __name__ == "__main__":
+    main();
