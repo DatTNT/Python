f3260158c57aa4bf965c9b923ff3121ed7ef0b0a
TienDat <dattnt24@gmail.com>
2021-08-08 23:34:54 +0700

    Add New File

---
+++ StringMethod.py
@@ -0,0 +1,16 @@
+
+def main():
+    name = "Tien Dat"
+
+    # print(len(name));
+    # print(name.find("n"))
+    # print(name.capitalize()) "in chu cai dau tien trong chuoi do"
+    # print(name.lower())
+    # print(name.isdigit()) "Xac dinh trong chuoi co phai la so khong"
+    # print(name.isalpha()); "Xac dinh trong chuoi nay hoan toan la chu ko" name = "TienDat"
+    # print(name.count("o"))
+    # print(name.replace("e","a"));
+    # print(name*3)
+
+if __name__ == "__main__":
+    main();
--- Variable.py
+++ Variable.py
@@ -40,16 +40,125 @@ def main():
 
     #List Python
 
-    list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
-    tinylist = [123, 'john']
+    # list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+    # tinylist = [123, 'john']
 
-    print(list)          # Prints complete list
-    print (list[0])       # Prints first element of the list
-    print (list[1:3])     # Prints elements starting from 2nd till 3rd 
-    print (list[2:])       # Prints elements starting from 3rd element
-    print (tinylist * 2)  # Prints list two times
-    print (list + tinylist) # Prints concatenated lists
+    # print(list)          # Prints complete list
+    # print (list[0])       # Prints first element of the list
+    # print (list[1:3])     # Prints elements starting from 2nd till 3rd 
+    # print (list[2:])       # Prints elements starting from 3rd element
+    # print (tinylist * 2)  # Prints list two times
+    # print (list + tinylist) # Prints concatenated lists
+
+    #Tuples Python Là 1 giá trị bất biến khi khai báo vào không thể thay đổi được
+
+    # tuple = ( 'abcd', 786 , 2.23, 'john', 70.2  )
+    # tinytuple = (123, 'john')
+
+    # print (tuple)               # Prints the complete tuple
+    # print (tuple[0] )             # Prints first element of the tuple
+    # print (tuple[1:3] )           # Prints elements of the tuple starting from 2nd till 3rd 
+    # print (tuple[2:]  )           # Prints elements of the tuple starting from 3rd element
+    # print (tinytuple * 2)        # Prints the contents of the tuple twice
+    # print (tuple + tinytuple)     # Prints concatenated tuples
+
+    #Python Dictionary (associative"adj": Tính Liên kết, tập hợp) nó giống như 1 mảng tập hợp hoặc là 1 nó khá giống object
+    # dict = {}
+    # dict['one'] = "This is one"
+    # dict[2]     = "This is two"
+
+    # tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
 
 
+    # print (dict['one'])     # Prints value for 'one' key
+    # print (dict[2])           # Prints value for 2 key
+    # print (tinydict )         # Prints complete dictionary
+    # print (tinydict.keys())   # Prints all the keys
+    # print (tinydict.values()) # Prints all the values
+
+    1	
+# int(x [,base])
+
+# Converts x to an integer. base specifies the base if x is a string.
+
+# 2	
+# long(x [,base] )
+
+# Converts x to a long integer. base specifies the base if x is a string.
+
+# 3	
+# float(x)
+
+# Converts x to a floating-point number.
+
+# 4	
+# complex(real [,imag])
+
+# Creates a complex number.
+
+# 5	
+# str(x)
+
+# Converts object x to a string representation.
+
+# 6	
+# repr(x)
+
+# Converts object x to an expression string.
+
+# 7	
+# eval(str)
+
+# Evaluates a string and returns an object.
+
+# 8	
+# tuple(s)
+
+# Converts s to a tuple.
+
+# 9	
+# list(s)
+
+# Converts s to a list.
+
+# 10	
+# set(s)
+
+# Converts s to a set.
+
+# 11	
+# dict(d)
+
+# Creates a dictionary. d must be a sequence of (key,value) tuples.
+
+# 12	
+# frozenset(s)
+
+# Converts s to a frozen set.
+
+# 13	
+# chr(x)
+
+# Converts an integer to a character.
+
+# 14	
+# unichr(x)
+
+# Converts an integer to a Unicode character.
+
+# 15	
+# ord(x)
+
+# Converts a single character to its integer value.
+
+# 16	
+# hex(x)
+
+# Converts an integer to a hexadecimal string.
+
+# 17	
+# oct(x)
+
+# Converts an integer to an octal string.
 if __name__ == "__main__":
     main();
